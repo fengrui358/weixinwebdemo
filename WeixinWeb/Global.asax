4a464632d03cc6db8496c6ddbf84960d88abc27b
奉锐 <fengr@action98.com>
2015-12-17 10:34:35 +0800

    初始化网站Demo

---
+++ WeixinWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeixinWeb.MvcApplication" Language="C#" %>
---
+++ WeixinWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeixinWeb.Startup))]
+namespace WeixinWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
