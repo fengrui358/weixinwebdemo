50a7f171e039fdd32cf13a10fc8f137f198a68fe
free_macPro <fengrui358@163.com>
2015-12-18 14:39:13 +0800

    暂时提交

---
+++ WeixinWeb/MessageHandlers/CustomMessageHandle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Senparc.Weixin.MP.MessageHandlers;
+
+namespace WeixinWeb.MessageHandlers
+{
+    public class CustomMessageHandle : MessageHandler<>
+    {
+    }
+}
