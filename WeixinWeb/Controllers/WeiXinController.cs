3d304fa794220f83d6df578be56ed65e62d53388
free <fengrui358@163.com>
2015-12-19 10:11:52 +0800

    加入一个微信测试页面

---
+++ WeixinWeb/Controllers/WeiXinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeixinWeb.Controllers
+{
+    public class WeiXinController : Controller
+    {
+        // GET: WeiXin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
